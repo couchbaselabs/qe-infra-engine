3bf451c4c056f10d8ff327342a60706fc383ab33
Raghav S K <raghav.sk@couchbase.com>
2024-04-06 13:10:39 +0530

    Removing RemoteConnectionHelper

--- helper/RemoteConnectionHelper/platforms/Linux/RPMBased/SUSELinux/suse_helper.py
+++
@@ -1,9 +0,0 @@
-from helper.RemoteConnectionHelper.platforms.Linux.RPMBased.rpm_helper import RPMHelper
-class SUSEHelper(RPMHelper):
-    def __init__(self, ipaddr, ssh_username, ssh_password) -> None:
-        super().__init__(ipaddr, ssh_username, ssh_password)
-        raise NotImplementedError("The helper has to be implemented")
-
-    def __del__(self):
-        super().__del__()
-    
