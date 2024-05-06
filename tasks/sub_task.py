69148d712bf70c4c4dfad455623b0403caab38f1
Raghav S K <raghav.sk@couchbase.com>
2024-05-07 01:24:06 +0530

    Added Task and subtask for node health monitor

--- tasks/sub_task.py
+++ tasks/sub_task.py
@@ -4,22 +4,26 @@ import time
 import logging
 from constants.task_states import SubTaskStates
 
-class SubTask(Future):
+class SubTask:
     def __init__(self, sub_task_name):
         self.sub_task_name = sub_task_name
         self.id = uuid.uuid4()
         self.exception = None
         self.start_time = None
         self.end_time = None
-        self.logger = logging.getLogger("tasks")
+        self.logger = logging.getLogger("subtasks")
         self.result = False
         self.result_json = None
         self.state = SubTaskStates.CREATED
 
     def start_sub_task(self):
+        self.logger.debug(f"Starting execution of {self.sub_task_name}_{self.id}")
         self.state = SubTaskStates.RUNNING
 
     def set_exception(self, exception):
+        if not isinstance(exception, Exception):
+            exception = Exception(exception)
+        self.logger.error(exception)
         self.exception = exception
         self.complete_sub_task(result=False)
         raise self.exception
--- tasks/task.py
+++ tasks/task.py
@@ -3,7 +3,7 @@ import time
 import concurrent
 import uuid
 from constants.task_states import TaskStates
-from task.sub_task import SubTask
+from tasks.sub_task import SubTask
 class Task:
     def __init__(self, task_name, max_workers):
         self.task_name = task_name
@@ -15,7 +15,7 @@ class Task:
         self.result = False
         self.result_json = None
         self.state = TaskStates.CREATED
-        self.subtasks = []
+        self.subtasks = {}
         self.executor_pool = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
 
     def start_task(self):
@@ -25,7 +25,10 @@ class Task:
         self.result = result
 
     def set_exception(self, exception):
+        if not isinstance(exception, Exception):
+            exception = Exception(exception)
         self.exception = exception
+        self.logger.error(exception)
         self.complete_task(result=False)
         raise self.exception
 
@@ -35,17 +38,19 @@ class Task:
         self.set_result(result=result)
 
     def add_sub_task(self, subtask:SubTask):
-        self.executor_pool.submit(subtask.execute_sub_task)
+        self.logger.debug(f"Sub task {subtask.sub_task_name}_{subtask.id} added for execution")
+        future_instance = self.executor_pool.submit(subtask.execute_sub_task)
+        self.subtasks[subtask] = future_instance
 
     def get_sub_task_result(self, subtask:SubTask):
         result = False
         try:
-            result = subtask.result()
-            exception = subtask.exception
+            result = self.subtasks[subtask].result()
+            exception = self.subtasks[subtask].exception()
             if exception:
-                self.logger.critical(f"Exception in {subtask.sub_task_name}_{subtask.uuid}: {exception}")
-        except Exception:
-            self.logger.warning(f"{subtask.sub_task_name}_{subtask.uuid} has not run properly and has ended abruptly")
+                self.logger.critical(f"Exception in {subtask.sub_task_name}_{subtask.id}: {exception}")
+        except Exception as e:
+            self.logger.warning(f"{subtask.sub_task_name}_{subtask.id} has not run properly and has ended abruptly : {e}")
         return result
 
     def generate_json_result(self):
