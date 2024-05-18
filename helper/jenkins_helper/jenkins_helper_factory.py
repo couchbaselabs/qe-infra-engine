c3967ff65c4e99578373d620b8d34339babf3e6f
Raghav S K <raghav.sk@couchbase.com>
2024-05-19 02:56:57 +0530

    Adding add slave functionality in jenkins helper

--- helper/jenkins_helper/jenkins_helper_factory.py
+++ helper/jenkins_helper/jenkins_helper_factory.py
@@ -1,17 +1,5 @@
-import sys
-import os
-
-# Adding project path into the sys paths for scanning all the modules
-script_dir = os.path.dirname(os.path.realpath(__file__))
-project_paths = [
-    os.path.join(script_dir, "..", ".."),
-    os.path.join(script_dir, "..", "..", "util", "ssh_util")
-]
-for project_path in project_paths:
-    if project_path not in sys.path:
-        sys.path.append(project_path)
-
 import logging
+import constants.jenkins as constants
 from helper.jenkins_helper.qe_jenkins_helper import QEJenkinsHelper
 from helper.jenkins_helper.qa_jenkins_helper import QAJenkinsHelper
 
@@ -19,14 +7,11 @@ logger = logging.getLogger("helper")
 
 class JenkinsHelperFactory:
 
-    QA_JENKINS = "qa_jenkins"
-    QE_JENKINS = "qe_jenkins"
-
     @staticmethod
     def fetch_helper(name):
-        if name == JenkinsHelperFactory.QA_JENKINS:
+        if name == constants.QA_JENKINS:
             return QAJenkinsHelper()
-        elif name == JenkinsHelperFactory.QE_JENKINS:
+        elif name == constants.QE_JENKINS:
             return QEJenkinsHelper()
         else:
             error_msg = "Illegal jenkins host name"
