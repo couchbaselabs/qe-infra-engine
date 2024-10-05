c0df4ede92add52dc406af0d1f201e685b941c43
Raghav S K <raghav.sk@couchbase.com>
2024-10-06 01:09:14 +0530

    Improving helper to convert uuid into str and format the datetime

--- helper/sdk_helper/testdb_helper/task_pool_helper.py
+++ helper/sdk_helper/testdb_helper/task_pool_helper.py
@@ -1,6 +1,7 @@
 import os
 import threading
 import copy
+from datetime import datetime
 from util.sdk_util.sdk_client import SDKClient
 from helper.sdk_helper.testdb_helper.test_db_helper import TestDBSDKHelper
 from helper.sdk_helper.sdk_helper import SingeltonMetaClass
@@ -45,7 +46,7 @@ class TaskPoolSDKHelper(TestDBSDKHelper, metaclass=SingeltonMetaClass):
 
     def create_task_doc(self, task_id, task_name):
         task_doc = copy.deepcopy(TASK_TEMPLATE)
-        task_doc["task_id"] = task_id
+        task_doc["task_id"] = str(task_id)
         task_doc["task_name"] = task_name
         task_doc["start_time"] = ""
         task_doc["end_time"] = ""
@@ -62,17 +63,18 @@ class TaskPoolSDKHelper(TestDBSDKHelper, metaclass=SingeltonMetaClass):
 
     def update_task_started(self, task_id, start_time):
         try:
-            task_doc = eval(self.get_doc(task_id))
+            task_doc = eval(self.get_doc(client=self.tasks_doc_connection,
+                                         key=str(task_id)))
         except Exception as e:
-            msg = f"Error fetching document from task with id {task_id} : {e}"
+            msg = f"Error fetching document from task with id {str(task_id)} : {e}"
             self.logger.error(msg)
             raise Exception(msg)
 
-        task_doc["start_time"] = start_time
+        task_doc["start_time"] = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
         task_doc["state"] = TaskStates.RUNNING
 
         return self.upsert_doc(client=self.tasks_doc_connection,
-                               key=task_id,
+                               key=str(task_id),
                                doc=task_doc,
                                bucket_name=self.task_pool_bucket_name,
                                scope=self.tasks_doc_scope_name,
@@ -80,18 +82,19 @@ class TaskPoolSDKHelper(TestDBSDKHelper, metaclass=SingeltonMetaClass):
 
     def update_task_completed(self, task_id, end_time, result):
         try:
-            task_doc = eval(self.get_doc(task_id))
+            task_doc = eval(self.get_doc(client=self.tasks_doc_connection,
+                                         key=str(task_id)))
         except Exception as e:
-            msg = f"Error fetching document from task with id {task_id} : {e}"
+            msg = f"Error fetching document from task with id {str(task_id)} : {e}"
             self.logger.error(msg)
             raise Exception(msg)
 
-        task_doc["end_time"] = end_time
+        task_doc["end_time"] = datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
         task_doc["state"] = TaskStates.COMPLETED
         task_doc["result"] = result
 
         return self.upsert_doc(client=self.tasks_doc_connection,
-                               key=task_id,
+                               key=str(task_id),
                                doc=task_doc,
                                bucket_name=self.task_pool_bucket_name,
                                scope=self.tasks_doc_scope_name,
@@ -99,10 +102,10 @@ class TaskPoolSDKHelper(TestDBSDKHelper, metaclass=SingeltonMetaClass):
 
     def add_results_to_task(self, task_id, result):
         result_doc = copy.deepcopy(TASK_RESULT_TEMPLATE)
-        result_doc["task_id"] = task_id
+        result_doc["task_id"] = str(task_id)
         result_doc["result"] = result
 
-        key = task_id
+        key = str(task_id)
         return self.upsert_doc(client=self.results_doc_connection,
                                key=key,
                                doc=result_doc,
